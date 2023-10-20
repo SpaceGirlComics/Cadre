360f1c75ee6dc5ce21eb75e3a9f5f45e92a26c7b
Chris Turner <mackenzie17@hotmail.com>
2023-10-20 10:00:29 -0300

    Add project files.

---
+++ JSONX/json.c
@@ -0,0 +1,341 @@
+#include "json.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+LPJSONOBJECT jsonArray(int count, char* json)
+{
+	// LPJSONOBJECT job = new JSONOBJECT;														//	c++
+	LPJSONOBJECT job = (LPJSONOBJECT)malloc(sizeof(JSONOBJECT));								// C
+	LPJSONOBJECT ret = job;
+	job->datalength = 0;
+	job->name = (char*)count;
+	job->type = JSON_NULL;
+	job->value = (void*)0;
+	int hold;
+
+	for (unsigned int x = 0; x <= strlen(json); x++)
+	{
+		switch (json[x])
+		{
+		case '\"':
+		{
+			x++;
+			job->type = JSON_STRING;
+			int y = 0;
+			while (json[x + y] != '\"')
+			{
+				if (json[x + y] == '\\')
+				{
+					y++;
+					if (json[x + y] == 'u')
+					{
+						y += 4;
+					}
+				}
+				y++;
+			}
+			//job->value = new char[y + 1];														//	c++
+			job->value = (void*)malloc(y + 1);													// C
+			memcpy((char*)job->value, &json[x], y);
+			((char*)job->value)[y] = '\0';
+			x += y;
+			job->datalength = y + 1;
+			break;
+		}
+
+		case '{':
+		{
+			job->type = JSON_OBJECT;
+			job->value = jsonParse(&json[++x]);
+			break;
+		}
+
+		case '[':
+		{
+			job->type = JSON_ARRAY;
+			job->value = jsonArray(0, json);
+			break;
+		}
+
+		case 't':
+		{
+			job->type = JSON_BOOLEAN;
+			if (json[++x] == 'r' && json[++x] == 'u' && json[++x] == 'e')
+			{
+				//job->value = new bool;														//	c++
+				//*((bool*)job->value) = true;													//	c++
+				job->value = (void*)malloc(sizeof(unsigned char));								//	C
+				*((unsigned char*)job->value) = 1;
+				job->datalength = sizeof(unsigned char);
+			}
+			else
+			{
+				job->type = JSON_MALFORMED;
+				return((LPJSONOBJECT)JSON_NULL);
+			}
+			break;
+		}
+
+		case 'f':
+		{
+			job->type = JSON_BOOLEAN;
+			if (json[++x] == 'a' && json[++x] == 'l' && json[++x] == 's' && json[++x] == 'e')
+			{
+				//job->value = new bool;														//	c++
+				//*((bool*)job->value) = false;													//	c++
+				job->value = (void*)malloc(sizeof(unsigned char));								//	C
+				*((unsigned char*)job->value) = 0;
+				job->datalength = sizeof(unsigned char);
+			}
+			else
+			{
+				job->type = JSON_MALFORMED;
+				return((LPJSONOBJECT)JSON_NULL);
+			}
+			break;
+		}
+
+		case 'n':
+		{
+			job->type = JSON_NULL;
+			if (json[++x] == 'u' && json[++x] == 'l' && json[++x] == 'l')
+			{
+				job->value = (void*)0;
+			}
+			else
+			{
+				job->type = JSON_MALFORMED;
+				return((LPJSONOBJECT)JSON_NULL);
+			}
+			break;
+		}
+
+		case '}':
+		case ':':
+		case ' ':
+		case '\n':
+		case '\r':
+		case '\t':
+		{
+			x++;
+			break;
+		}
+
+		case ',':
+		{
+			// job->next = new JSONOBJECT;														//	c++
+			job->next = (LPJSONOBJECT)malloc(sizeof(JSONOBJECT));								// C
+			job = job->next;
+			job->name = (char*)count;
+			break;
+		}
+
+		case ']':
+		{
+			job->datalength = count;
+			return(job);
+			break;
+		}
+
+		default:
+		{
+			if ((json[x] >= 48 && json[x] <= 57) || json[x] == 45)
+			{
+				job->type = JSON_NUMBER;
+				//job->value = new double;													// c++
+				job->value = (void*)malloc(sizeof(double));									// C
+				hold = sscanf(&json[x], "%lf", (double*)job->value);
+				job->datalength = sizeof(double);
+				while ((json[x] >= 48 && json[x] <= 57) || json[x] == 45)
+				{
+					x++;
+				}
+				x--;
+			}
+			break;
+		}
+		}
+	}
+
+	//delete(job);																				// c++
+	free(job);																					// C
+	return((LPJSONOBJECT)JSON_NULL);
+}
+
+LPJSONOBJECT jsonParse(char* json)
+{
+	// LPJSONOBJECT job = new JSONOBJECT;														//	c++
+	LPJSONOBJECT job = (LPJSONOBJECT)malloc(sizeof(JSONOBJECT));								// C
+	LPJSONOBJECT ret = job;
+	job->datalength = 0;
+	job->name = (char*)0;
+	job->type = JSON_NULL;
+	job->value = (void*)0;
+	int hold;
+
+	for (unsigned int x = 0; x <= strlen(json); x++)
+	{
+		switch (json[x])
+		{
+		case '\"':
+		{
+			x++;
+			job->type = JSON_STRING;
+			int y = 0;
+			while (json[x + y] != '\"')
+			{
+				if (json[x + y] == '\\')
+				{
+					y++;
+					if (json[x + y] == 'u')
+					{
+						y += 4;
+					}
+				}
+				y++;
+			}
+			//job->value = new char[y + 1];														//	c++
+			job->value = (void*)malloc(y + 1);													// C
+			memcpy((char*)job->value, &json[x], y);
+			((char*)job->value)[y] = '\0';
+			x += y;
+			job->datalength = y + 1;
+			break;
+		}
+		case '{':
+		{
+			job->type = JSON_OBJECT;
+			job->value = jsonParse(&json[++x]);
+			break;
+		}
+		case '[':
+		{
+			job->type = JSON_ARRAY;
+			job->value = jsonArray(0, &json[++x]);
+			break;
+		}
+		case 't':
+		{
+			job->type = JSON_BOOLEAN;
+			if (json[++x] == 'r' && json[++x] == 'u' && json[++x] == 'e')
+			{
+				//job->value = new bool;														//	c++
+				//*((bool*)job->value) = true;													//	c++
+				job->value = (void*)malloc(sizeof(unsigned char));								//	C
+				*((unsigned char*)job->value) = 1;
+				job->datalength = sizeof(unsigned char);
+			}
+			else
+			{
+				job->type = JSON_MALFORMED;
+				return((LPJSONOBJECT)JSON_NULL);
+			}
+			break;
+		}
+		case 'f':
+		{
+			job->type = JSON_BOOLEAN;
+			if (json[++x] == 'a' && json[++x] == 'l' && json[++x] == 's' && json[++x] == 'e')
+			{
+				//job->value = new bool;														//	c++
+				//*((bool*)job->value) = false;													//	c++
+				job->value = (void*)malloc(sizeof(unsigned char));								//	C
+				*((unsigned char*)job->value) = 0;
+				job->datalength = sizeof(unsigned char);
+			}
+			else
+			{
+				job->type = JSON_MALFORMED;
+				return((LPJSONOBJECT)JSON_NULL);
+			}
+			break;
+		}
+		case 'n':
+		{
+			job->type = JSON_NULL;
+			if (json[++x] == 'u' && json[++x] == 'l' && json[++x] == 'l')
+			{
+				job->value = (void*)0;
+			}
+			else
+			{
+				job->type = JSON_MALFORMED;
+				return((LPJSONOBJECT)JSON_NULL);
+			}
+			break;
+		}
+		case ':':
+		{
+			job->name = (char*)job->value;
+			job->value = (void*)0;
+			job->type = JSON_NULL;
+			job->datalength = 0;
+			break;
+		}
+		case ' ':
+		case '\n':
+		case '\r':
+		case '\t':
+		{
+			x++;
+			break;
+		}
+
+		case ',':
+		{
+			// job->next = new JSONOBJECT;														//	c++
+			job->next = (LPJSONOBJECT)malloc(sizeof(JSONOBJECT));								// C
+			job = job->next;
+			break;
+		}
+
+		case '}':
+		{
+			return(ret);
+			break;
+		}
+		default:
+		{
+			if ((json[x] >= 48 && json[x] <= 57) || json[x] == 45)
+			{
+				job->type = JSON_NUMBER;
+				//job->value = new double;														// c++
+				job->value = (void*)malloc(sizeof(double));										// C
+				hold = sscanf(&json[x], "%lf", (double*)job->value);
+				job->datalength = sizeof(double);
+				while ((json[x] >= 48 && json[x] <= 57) || json[x] == 45)
+				{
+					x++;
+				}
+				x--;
+			}
+			break;
+		}
+		}
+	}
+
+	//delete(job);																					// c++
+	free(job);																						// C
+	return((LPJSONOBJECT)JSON_NULL);
+}
+
+enum JSONTYPE jsonGet(LPJSONOBJECT source, void* destination)
+{
+	return(JSON_NULL);
+}
+
+enum JSONTYPE jsonPathGet(LPJSONOBJECT source, void* destination, char* path)
+{
+	return(JSON_NULL);
+}
+
+enum JSONTYPE jsonEncode(LPJSONOBJECT json, char* output)
+{
+	return(JSON_NULL);
+}
+
+enum JSONTYPE jsonDestroy(LPJSONOBJECT json)
+{
+	return(JSON_NULL);
+}
---
+++ JSONX/json.h
@@ -0,0 +1,26 @@
+#ifndef JSON_H
+#define JSON_H
+
+enum JSONTYPE { JSON_NULL, JSON_STRING, JSON_NUMBER, JSON_OBJECT, JSON_ARRAY, JSON_BOOLEAN, JSON_MALFORMED };
+typedef struct _JSONOBJECT
+{
+	char* name;
+	enum JSONTYPE type;
+	void* value;
+	int datalength;
+	struct _JSONOBJECT* next;
+} JSONOBJECT, * LPJSONOBJECT;
+
+/**
+* creates a Javascript object from a JSON formatted string.
+* Objects created with this function must be deleted using the jsonDestroy function
+* @param json a string containing JSON formatted text
+* @returns on success, a pointer to the new JSONOBJECT, otherwise, JSON_NULL
+*/
+LPJSONOBJECT jsonParse(char* json);
+enum JSONTYPE jsonGet(LPJSONOBJECT source, void* destination);
+enum JSONTYPE jsonPathGet(LPJSONOBJECT source, void* destination, char* path);
+enum JSONTYPE jsonEncode(LPJSONOBJECT json, char* output);
+enum JSONTYPE jsonDestroy(LPJSONOBJECT json);
+
+#endif
